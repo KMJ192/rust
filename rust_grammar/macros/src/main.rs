399b9f0444d3eac733e2cb64ad8a9891649210fc
KimMyeongJun <myeongjun222@naver.com>
2020-07-15 22:43:28 +0900

    rust

--- rust_grammar/macros/src/main.rs
+++ rust_grammar/macros/src/main.rs
@@ -41,6 +41,64 @@ macro_rules! exam{
     );
 }
 
+macro_rules! compr {
+    ($id1: ident | $id2: ident <- [$start : expr ; $end : expr], $cond:expr) => {
+        {
+            let mut vec = Vec::new();
+
+            for num in $start..$end + 1 {
+                if $cond(num){
+                    vec.push(num);
+                }
+            }
+            
+            vec
+        }
+    };
+}
+
+use std::collections::HashMap;
+
+macro_rules! new_map {
+    ($($key: expr => $val: expr), *) => {
+        {
+            let mut map = HashMap::new();
+
+            $(
+                map.insert($key, $val);
+            )*
+            
+            map
+        }
+    };
+}
+
+macro_rules! calc {
+    (eval $e:expr) => {
+        {
+            {
+                let val : usize = $e;
+                println!("{} = {}", stringify!{$e}, val);
+            }
+        }
+    };
+
+    (eval $e:expr, $(eval $es:expr), +) => {
+        {
+            calc! {eval $e}
+            calc! { $(eval $es), +}
+        }
+    };
+}
+
+fn even(x : i32) -> bool{
+    x%2 == 0
+}
+
+fn odd(x : i32) -> bool {
+    x%2 != 0
+}
+
 fn main() {
     a_macro!();
 
@@ -57,5 +115,25 @@ fn main() {
     });
 
     exam!(1 == 1; and 2 == 1 + 2);
+    exam!(1 == 1; or 2 == 1 + 2);
+
+    let evens = compr![x | x <- [1;10], even];
+    let odds = compr!(y | y <- [1;10], odd);
+    println!("{:?}", evens);
+    println!("{:?}", odds);
+
+    let m = new_map!{
+        "one" => 1,
+        "two" => 2,
+        "three" => 3
+    };
+    println!("{:?}", m);
+
+    calc!{
+        eval 4 + 5,
+        eval 10 * 10,
+        eval (5 * 2) + 20
+        
+    }
 }
 
